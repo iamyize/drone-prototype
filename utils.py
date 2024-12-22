5ed36f1c2051dbf563c94de43389ff2fdf9515d4
Yize <yize@ahlab.org>
2024-12-22 20:48:28 +0800

    minor updates

--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+import pyttsx3
+
 def load_file(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip()
+
+def play_sound(sound):
+    wave_obj = sa.WaveObject.from_wave_file(sound)
+    play_obj = wave_obj.play()
+
+def init_tts_engine():
+    engine = pyttsx3.init()
+    return engine
+
+def speak(engine, message):
+    engine.say(message)
+    engine.runAndWait()
