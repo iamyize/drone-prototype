936f6f91bb9c4448cf69b0163ebd85d5ac689c03
Ibnu Taimiyyah <ibnu2651@gmail.com>
2024-12-26 01:48:45 +0800

    Change to rewrite code.txt instead of append and other restructuring

--- code_generation.py
+++ code_generation.py
@@ -2,9 +2,12 @@ import openai
 import time
 import datetime
 import utils
+import os
 
 participantid = input("Enter the participant's ID: ")
-ct = datetime.datetime.now()
+# ct = datetime.datetime.now()
+# Reformat cos windows files cannot have ":"
+ct = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     
 TEMPERATURE = 0.1
 LOG_FILE_PATH = f"logs/{participantid}_{ct}.txt"
@@ -14,10 +17,10 @@ def get_chatgpt_code(messages, api_key):
         begintime = time.time()
         client = openai.OpenAI(api_key=api_key)
         
-        # if isinstance(messages, str):
-        #     messages = [
-        #         {"role": "user", "content": messages}
-        #     ]
+        if isinstance(messages, str):
+            messages = [
+                {"role": "user", "content": messages}
+            ]
         
         # send request 
         completion = client.chat.completions.create(
@@ -31,9 +34,10 @@ def get_chatgpt_code(messages, api_key):
         messages.append({"role": "assistant", "content": output})
         print("ChatGPT: " + output)
         
-        with open('code.txt', 'a') as f:
+        with open('code.txt', 'w') as f:
             f.write(output)
 
+        os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
         with open(LOG_FILE_PATH, 'a') as f:
             f.write(f'ChatGPT Response Time: {elapsedtime}\nChatGPT: {output}\n\n\n')
         print("ChatGPT Response Time: " + str(elapsedtime))
@@ -51,12 +55,12 @@ if __name__ == "__main__":
     api_key = utils.load_file('api_key.txt')
     messages = utils.load_file('prompt.txt')
 
-    # Prompt initial msg
-    messages = [{"role": "developer", "content": messages}]
-
-    # Insert actl msg here
-    gpt_input = "here"
-    messages.append({"role": "user", "content": gpt_input})
+    # # Prompt initial msg
+    # messages = [{"role": "developer", "content": messages}]
+    #
+    # # Insert actl msg here
+    # gpt_input = "here"
+    # messages.append({"role": "user", "content": gpt_input})
 
     get_chatgpt_code(messages, api_key)
     print("Generation Done!")
